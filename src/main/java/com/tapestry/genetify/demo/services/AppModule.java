1271fbc4141a6d8a68b0e2649a42fcc5cea126b0
Nouredine Khadri <nourredine.k@gmail.com>
2013-04-10 16:45:01 +0200

    add config for the new kawwa theme

--- src/main/java/com/tapestry/genetify/demo/services/AppModule.java
+++ src/main/java/com/tapestry/genetify/demo/services/AppModule.java
@@ -1,7 +1,11 @@
 package com.tapestry.genetify.demo.services;
 
+import net.atos.kawwaportal.components.KawwaConstants;
+import net.atos.kawwaportal.components.services.Theme0Stack;
+
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.services.javascript.JavaScriptStack;
 import org.got5.tapestry5.jquery.JQuerySymbolConstants;
 
 
@@ -10,8 +14,12 @@ public class AppModule {
 		configuration.add(SymbolConstants.START_PAGE_NAME, "Index");
 		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");		
 		configuration.add(JQuerySymbolConstants.SUPPRESS_PROTOTYPE, "false");		
-		configuration.add(JQuerySymbolConstants.JQUERY_ALIAS, "$j");		
-		
+		configuration.add(JQuerySymbolConstants.JQUERY_ALIAS, "$j");
 	}
+	
+	public static void contributeJavaScriptStackSource(MappedConfiguration<String, JavaScriptStack> configuration)
+    {
+    	configuration.overrideInstance(KawwaConstants.STACK_ID, Theme0Stack.class);
+    }
 
 }

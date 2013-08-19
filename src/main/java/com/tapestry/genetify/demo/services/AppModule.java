df4a8d205e8d7b4b80a0d6c02d8a8b995a67c127
Nourredine Khadri <nourredine.k@gmail.com>
2013-08-19 17:54:54 +0200

    set production mode to true

--- src/main/java/com/tapestry/genetify/demo/services/AppModule.java
+++ src/main/java/com/tapestry/genetify/demo/services/AppModule.java
@@ -12,7 +12,7 @@ import org.got5.tapestry5.jquery.JQuerySymbolConstants;
 public class AppModule {
 	public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration) {
 		configuration.add(SymbolConstants.START_PAGE_NAME, "Index");
-		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");		
+		configuration.add(SymbolConstants.PRODUCTION_MODE, "true");		
 		configuration.add(JQuerySymbolConstants.SUPPRESS_PROTOTYPE, "false");		
 		configuration.add(JQuerySymbolConstants.JQUERY_ALIAS, "$j");
 	}
